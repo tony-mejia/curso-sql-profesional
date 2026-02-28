2b7cf41731ed7745625b23b1a6c9f71ae3e8e385
tony-mejia <antoniomerod96@gmail.com>
2026-02-27 22:07:25 -0600

    Añadido script SQL para eliminar registros de la tabla ingredientes.

---
+++ Trabajar con Datos/Delete.sql
@@ -0,0 +1,7 @@
+#DELETE se utiliza para eliminar registros existentes de una tabla
+
+DELETE FROM ingredientes 
+WHERE ingredientes_id = 11;
+
+DELETE FROM ingredientes 
+WHERE ingredientes_id BETWEEN 21 AND 22;
